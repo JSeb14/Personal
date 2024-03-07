77240c3f94be4188b4243ec62e7bf3832353f7e2
JSeb14 <158070529+JSeb14@users.noreply.github.com>
2024-03-07 15:33:30 -0600

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-let underline = (html) => {
-    html.setAttribute("style", "text-decoration: underline");
-}
-
-let removeUnderline = (html) => {
-    html.setAttribute("style", "text-decoration: none");
-}
